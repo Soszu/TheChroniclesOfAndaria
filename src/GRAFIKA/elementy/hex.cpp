119d1c84db25db30228ba24a1fee73e787c94c71
Rafał Soszyński <r.soszynski@student.uw.edu.pl>
2013-08-15 01:11:10 +0200

    Reorganizacja wszystkich plików 	przepisanie cmake'a 	usunięcie zasoby.qrc 	dodanie runTool.sh jako odpalacza kosmosu 	mała zmiana w .gitignore 	przeniesienie dokumentacji, notatek i ToDoList (i tak jej tu nie było :) do zewnętrznego katalogu

---
+++ src/GRAFIKA/elementy/hex.cpp
@@ -0,0 +1,151 @@
+﻿#include "hex.h"
+
+Hex::Hex(Pole* pole, qreal bok, ObszarPlanszy* obszar)
+{
+	this->bok = bok;
+	this->obszarPlanszy = obszar;
+	this->wysokosc = ObszarPlanszy::podajWysokosc(bok);
+	this->pole = pole;
+	setPos(ObszarPlanszy::podajSrodek(pole->getMiejsce(), bok));
+	podswietlenie = false;
+	zaznaczenie = false;
+	this->setToolTip(pole->getNazwa() + "\nKoszt ruchu: " + QString::number(pole->getWspolczynnik()));
+}
+
+/**
+ * @brief Hex::boundingRect	Zwraca prostokąt, stanowiący granice hexa.
+ * @return
+ */
+QRectF Hex::boundingRect() const
+{
+	QPointF poczatek(-wysokosc,-bok);
+	QSizeF wymiary(2 * wysokosc, 2 * bok);
+	return QRectF(poczatek, wymiary);
+}
+
+/**
+ * @brief Hex::shape	Zwraca dokładne ograniczenie hexa, sześciąkąt foremny, który zawiera hex.
+ * @return		QPainterPath z ograniczeniem hexa.
+ */
+QPainterPath Hex::shape()
+{
+	QPainterPath figura;
+	figura.addPolygon(QPolygonF(podajWierzcholki()));
+	figura.closeSubpath();
+	return figura;
+}
+
+/**
+ * @brief Hex::paint	Rysuje hex.
+ * @param painter
+ * @param option
+ * @param widget
+ */
+void Hex::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+	painter->setRenderHint(QPainter::SmoothPixmapTransform);
+	painter->drawImage(boundingRect(), QImage(PREFIX_HEXOW + pole->getPlikZObrazkiem()));
+	if(pole->getCzyPoleZPrzeciwnikiem())
+	{
+		QPointF przesuniecie( wysokosc * 0.7, bok * 0.3); //przesuniecie symbolu miecza względem początku
+		QRectF ograniczenieSymbolu(boundingRect().topLeft() + przesuniecie, boundingRect().size() * 0.3);
+
+		painter->drawImage(ograniczenieSymbolu, QImage(PLIK_Z_SYMBOLEM_WALKI));
+	}
+	if(pole->getCzyPoleZMiastem())
+	{
+		QPointF przesuniecie(0, bok * 1.24 ); //przesuniecie symbolu miecza względem początku
+		QRectF ograniczenieTekstu(boundingRect().topLeft() + przesuniecie, boundingRect().size());
+
+		QFont czcionka("OldEnglish", bok * 0.2, QFont::Bold);
+		if(pole->getNazwa().size() >= MAX_SIZE_PRZED_SCISKANIEM)
+			czcionka.setStretch(100 * MAX_SIZE_PRZED_SCISKANIEM / pole->getNazwa().size()); //zmniejsza szerokość liter
+		painter->setFont(czcionka);
+		painter->setPen(QPen(QColor(Qt::red)));
+		painter->drawText(ograniczenieTekstu, Qt::AlignHCenter, pole->getNazwa());
+	}
+	if(podswietlenie)
+		painter->fillPath(shape(),QBrush(Qt::gray, Qt::Dense7Pattern));
+	if(zaznaczenie)
+	{
+		painter->setPen(QPen(QBrush(Qt::white), bok / 10));
+		painter->drawPath(ksztaltZaznaczenia());
+	}
+}
+
+/**
+ * @brief Hex::setBok	Ustawia bok i wielkości od niego zależne, a następnie się przerysowuje
+ * @param bok		długość nowego boku
+ */
+void Hex::setBok(qreal bok)
+{
+	this->bok = bok;
+	this->wysokosc = ObszarPlanszy::podajWysokosc(bok);
+	setPos(ObszarPlanszy::podajSrodek(pole->getMiejsce(), bok));
+}
+
+/**
+ * @brief Hex::setPodswietlenie	Ustawia odpowiednią opcję podświetlenia, po czym się przerysowuje
+ * @param opcja
+ */
+void Hex::setPodswietlenie(bool opcja)
+{
+	this->podswietlenie = opcja;
+	update(boundingRect());
+}
+
+/**
+ * @brief Hex::podajWierzcholki		Zwraca wierzchołki hexa.
+ * @return	QVector wierzchołków hexa
+ */
+QVector<QPointF> Hex::podajWierzcholki()
+{
+	QVector<QPointF> wierzcholki;
+	wierzcholki << QPointF(0, -bok) << QPointF(wysokosc, -bok / 2) <<QPointF( wysokosc, bok / 2)
+		    << QPointF(0, bok ) << QPointF(-wysokosc, bok / 2) <<QPointF(-wysokosc, -bok/ 2);
+	return wierzcholki;
+}
+
+/**
+ * @brief Hex::zaznacz	Włącza podświetlenie danego hexa, po czym się przerysowywuje.
+ */
+void Hex::zaznacz()
+{
+	zaznaczenie = true;
+	update(boundingRect());
+}
+
+/**
+ * @brief Hex::odznacz	Wyłącza podświetlenie danego hexa, po czym się przerysowywuje.
+ */
+void Hex::odznacz()
+{
+	zaznaczenie = false;
+	update(boundingRect());
+}
+
+/**
+ * @brief Hex::mousePressEvent	Informuje o kliknięciu na siebie
+ * @param event
+ */
+void Hex::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+	obszarPlanszy->kliknietoHex(pole->getMiejsce());
+}
+
+/**
+ * @brief Hex::ksztaltZaznaczenia	Definuje sposób zaznaczenia hexa, reprezentowany jako QPainterPath
+ * @return	ścieżka stanowiąca sposób zaznaczenia hexa.
+ */
+QPainterPath Hex::ksztaltZaznaczenia()
+{
+	QPainterPath krzyz;
+	krzyz.moveTo(QPointF(0, -bok));
+	krzyz.lineTo(QPointF(0, bok ));
+	krzyz.moveTo(QPointF(wysokosc, -bok / 2));
+	krzyz.lineTo(QPointF(-wysokosc, bok /2));
+	krzyz.moveTo(QPointF( wysokosc, bok / 2));
+	krzyz.lineTo(QPointF(-wysokosc, -bok/2));
+
+	return krzyz;
+}
---
+++ src/GRAFIKA/elementy/hex.h
@@ -0,0 +1,39 @@
+﻿#ifndef HEX_H
+#define HEX_H
+
+#include <QGraphicsItem>
+#include <QPainter>
+#include <QVector>
+#include "obszarplanszy.h"
+#include "pole.h"
+#include "gra.h"
+
+class ObszarPlanszy;
+
+class Hex : public QGraphicsItem
+{
+public:
+	explicit Hex(Pole* pole, qreal bok, ObszarPlanszy* obszar);
+	QRectF boundingRect() const;
+	QPainterPath shape();
+	void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+	void setBok(qreal bok);
+	void setPodswietlenie(bool opcja);
+	void zaznacz();
+	void odznacz();
+private:
+	static const int MAX_SIZE_PRZED_SCISKANIEM = 8;
+	qreal bok;
+	qreal wysokosc;
+	Pole* pole;
+	ObszarPlanszy* obszarPlanszy;
+	bool podswietlenie;
+	bool zaznaczenie;
+	QPainterPath ksztaltZaznaczenia();
+	QVector<QPointF> podajWierzcholki();
+
+protected:
+	void mousePressEvent(QGraphicsSceneMouseEvent *event);
+};
+
+#endif // HEX_H
